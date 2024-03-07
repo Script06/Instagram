f22766cd14fdc74800b203d98c57ee23db177ab1
Alexandr Gololobov <gololobovac@gmail.com>
2024-03-07 19:44:04 +0300

    delete column photo from table posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_07_154957) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_07_161935) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -41,7 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_07_154957) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.text "photo"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
