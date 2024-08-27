92a04ded58ab8ed903bd19cf5b6ed25f55c83e05
Gololobov Alexandr <gololobovac@gmail.com>
2024-08-27 21:33:06 +0300

    небольшие исправления + бэкап перед изменением post_like_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,5 @@ class PostsController < ApplicationController
   # для ленты постов
   def index
     @posts = Post.all
-    @comment = Comment.new
   end
 end
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,12 +1,12 @@
 class SubscriptionsController < ApplicationController
   def create
     Subscription.create(follower_id: current_user.id, following_id: params[:id])
+
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    subscription = Subscription.find_by(follower_id: current_user.id, following_id: params[:id])
-    subscription&.destroy
+    Subscription.find_by(follower_id: current_user.id, following_id: params[:id])&.destroy
 
     redirect_back(fallback_location: root_path)
   end
