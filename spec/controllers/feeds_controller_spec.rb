d779410d4926e86f26f9305339be3e82f8aac3a4
Script06 <gololobovac@gmail.com>
2024-10-12 15:22:48 +0300

    удалены лишние коментарии в feeds_controller_spec

--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
-# TODO: Проверить:
-# 1) Что возвращаются посты только тех, на кого подписан
-# 2) Что возвращается в случае отсуствия подписок?
-# НАДО:
+
 RSpec.describe FeedsController, type: :controller do
   describe '#index' do
     subject { get :index }
 
     context 'when user is authenticated' do
-      let(:current_user) { create(:user) }
+      let(:user) { create(:user) }
       let(:following_user) { create(:user) }
       let(:not_following_user) { create(:user) }
 
       before do
-        sign_in current_user
+        sign_in user
       end
 
       it 'should return 200' do
@@ -23,12 +20,12 @@ RSpec.describe FeedsController, type: :controller do
 
       it 'should return posts only following_user ' do
         5.times do
-          create(:post, user: current_user)
+          create(:post, user:)
           create(:post, user: following_user)
           create(:post, user: not_following_user)
         end
 
-        current_user.follow(following_user)
+        user.follow(following_user)
 
         subject
 
